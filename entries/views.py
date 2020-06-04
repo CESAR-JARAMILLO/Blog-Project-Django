839a7735dfcd9909957c48cba7e117df5137c4b3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-03 23:12:58 -0700

    created detail view

--- entries/views.py
+++ entries/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Django's built in Class Generic Views import
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Entry
 
 class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
+    # Designates the name of the variable to use in the context.
+    context_object_name = 'blog_entries'
+
+class EntryView(DetailView):
+    model = Entry
+    template_name = 'entries/entry_detail.html'
