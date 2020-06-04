d748a891a23742cf470c41c43bb7c5b479ff032d
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-04 12:39:56 -0700

    deployment prep

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$lc#)asud6ofjal!j&^6a915jcnw7gy+3f^^_wl41lr_1)u(av'
+SECRET_KEY = os.environ.get('BLOG_KEY')
+# '$lc#)asud6ofjal!j&^6a915jcnw7gy+3f^^_wl41lr_1)u(av'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +127,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
